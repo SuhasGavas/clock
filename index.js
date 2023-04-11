3eca5b758922af263cf4de04f6854c3992791437
SuhasGavas <suhasgavas114@gmail.com>
2023-04-11 09:49:30 +0530

    Second draft

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-//Initialize variables
-let seconds = -1; 
-let minutes = 0;
-let hours   = 0;
+// Initialize variables
+// let seconds = -1;
+// let minutes = 0;
+// let hours   = 0;
 
-//This function increaments the seconds, minutes and hours
-function process() {
-  if (seconds < 59) {
-    seconds++;
-  } else {
-    if (minutes < 59) {
-      minutes++;
-      seconds = 0;
-    } else {
-      if (condition) {
-        hours++;
-        minutes = 0;
-        seconds = 0;
-      }
-    }
-  }
+// //This function increaments the seconds, minutes and hours
+// function process() {
+//   if (seconds < 59) {
+//     seconds++;
+//   } else {
+//     if (minutes < 59) {
+//       minutes++;
+//       seconds = 0;
+//     } else {
+//       if (condition) {
+//         hours++;
+//         minutes = 0;
+//         seconds = 0;
+//       }
+//     }
+//   }
 
-  //Log
-  console.log(
-    "Hours : ",
-    hours,
-    " Minutes : ",
-    minutes,
-    " Seconds : ",
-    seconds
-  );
+//   //Log
+//   console.log(
+//     "Hours : ",
+//     hours,
+//     " Minutes : ",
+//     minutes,
+//     " Seconds : ",
+//     seconds
+//   );
+// }
+
+// setInterval(process, 1000);
+/* ----------------------------------------*/
+
+function getTime() {
+  let dateTime = new Date();
+  let hours = dateTime.getHours();
+  let minutes = dateTime.getMinutes();
+  let seconds = dateTime.getSeconds();
+
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
 }
 
-setInterval(process, 1000);
+setInterval(getTime, 10);
